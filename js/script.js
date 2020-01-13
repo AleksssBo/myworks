edb98e25ae9c3f69d7c34faee47f3c98e36a1976
Богдан <Bogdan@MacBook-Pro-Spawn.local>
2020-01-13 23:09:56 +0300

    my site portfolio

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    $('.btn-menu').on('click', function() {
+        $('aside').css("transform", "translateX(0px)")
+    });
+
+    $('.exit').on('click', function() {
+        $('aside').css("transform", "translateX(-275px)")
+    });
+
+    $("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+    $('a[href^="#"]').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+         scrollTop: $(anchor.attr("href")).offset().top
+        }, 1000);
+        e.preventDefault();
+
+        $('aside').css("transform", "translateX(-275px)")
+       });
+       return false;
+  
+  });
